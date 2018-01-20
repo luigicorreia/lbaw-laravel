07aafafc09943e12b395412a58b6716bcd7dbf98
Tiago Boldt Sousa <tiagoboldt@gmail.com>
2018-01-20 00:33:02 +0000

    stop docker postgresql when dev server is stoped (ctrl+c)

--- run.sh
+++ run.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-# Ensure laravel and dependencies are installed. Requires composer
-composer global require "laravel/installer"
-cd laravel-demo && composer install && cd ..
-
 # Install and run postgres locally
 docker build -t lbaw .
-docker run -p 5432:5432 -v pgdata:/var/lib/postgresql/data lbaw
+docker run --name lbaw -p 5432:5432 -v pgdata:/var/lib/postgresql/data -d lbaw
+
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+  docker stop lbaw
+  docker rm lbaw
+}
+
+# Ensure laravel and dependencies are installed. Requires composer
+composer global require "laravel/installer"
+cd laravel
+composer install
+php artisan serve
+
+docker stop lbaw
+docker rm lbaw
